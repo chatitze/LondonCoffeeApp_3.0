7fc093325524d5c48cdb1bb33a36d6d9c5eb51c8
Chatitze Moumin <chatitze.moumin@gmail.com>
2016-12-05 14:29:04 +0000

    first commit

---
+++ app/src/main/java/com/chatitzemoumin/londoncoffeeapp/ui/CoffeeShopListActivity.java
@@ -0,0 +1,19 @@
+package com.chatitzemoumin.londoncoffeeapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chatitzemoumin.londoncoffeeapp.R;
+
+/**
+ * Created by Chatitze Moumin on 17/11/14.
+ */
+
+public class CoffeeShopListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_shop_list);
+    }
+}
